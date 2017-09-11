d7f1529ab4d0845b6ad825df341a9cca0008342d
Michiel Gerritsen <michiel@controlaltdelete.nl>
2017-09-11 07:36:23 +0200

    Initial commit

---
+++ Block/AddJavascript.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ *    ______            __             __
+ *   / ____/___  ____  / /__________  / /
+ *  / /   / __ \/ __ \/ __/ ___/ __ \/ /
+ * / /___/ /_/ / / / / /_/ /  / /_/ / /
+ * \______________/_/\__/_/   \____/_/
+ *    /   |  / / /_
+ *   / /| | / / __/
+ *  / ___ |/ / /_
+ * /_/ _|||_/\__/ __     __
+ *    / __ \___  / /__  / /____
+ *   / / / / _ \/ / _ \/ __/ _ \
+ *  / /_/ /  __/ /  __/ /_/  __/
+ * /_____/\___/_/\___/\__/\___/
+ *
+ */
+
+namespace ControlAltDelete\LiveReload\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class AddJavascript extends Template
+{
+    public $_template = 'Javascript.phtml';
+    /**
+     * @var \Magento\Framework\App\State
+     */
+    private $appState;
+
+    /**
+     * @param Template\Context $context
+     * @param \Magento\Framework\App\State $appState
+     * @param array $data
+     */
+    public function __construct(
+        Template\Context $context,
+        \Magento\Framework\App\State $appState,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->appState = $appState;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHostUrl()
+    {
+        $scheme = $_SERVER['SERVER_PORT'] == 443 ? 'https' : 'http';
+
+        return $scheme . '://' . $_SERVER['HTTP_HOST'] . ':35729';
+    }
+
+    /**
+     * Only output the HTML if we are not in production mode
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if ($this->_appState->getMode() == \Magento\Framework\App\State::MODE_PRODUCTION) {
+            return '';
+        }
+
+        return parent::_toHtml();
+    }
+}
